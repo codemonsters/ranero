d2b9eec6be124db30ced55366b48d088a5ee7fee
asexto <a.sexto@gmail.com>
2019-03-21 18:40:09 +0100

    Commit inicial

---
+++ desktop/src/es/codemonsters/ranero/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.codemonsters.ranero.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.codemonsters.ranero.Ranero;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Ranero(), config);
+	}
+}
