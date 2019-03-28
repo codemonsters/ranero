f8e91c3f2ac0077af47a5f2569f0ed8db4213483
Autotren <kenta.bussi@gmx.com>
2019-03-28 19:22:51 +0100

    Menu feo añadido. Fuentes creadas. Viva España

--- core/src/es/codemonsters/ranero/Ranero.java
+++ core/src/es/codemonsters/ranero/Ranero.java
@@ -3,23 +3,42 @@ package es.codemonsters.ranero;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 import es.codemonsters.ranero.screens.MenuScreen;
+import sun.font.TrueTypeFont;
 
 public class Ranero extends Game {
 	public SpriteBatch batch;
-	public BitmapFont font;
+	public BitmapFont fontBig;
+	public BitmapFont fontMedium;
+	FreeTypeFontGenerator generator;
+	FreeTypeFontGenerator.FreeTypeFontParameter parameter;
+
 
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		font = new BitmapFont();	//Use LibGDX's default Arial font.
-		this.setScreen(new MenuScreen(this));
+
+		generator = new FreeTypeFontGenerator(Gdx.files.internal("ComicRelief.ttf"));
+		parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+		parameter.size = 100;
+
+		fontBig = generator.generateFont(parameter);
+		fontBig.setColor(Color.BLACK);
+
+		parameter.size = 40;
+		fontMedium = generator.generateFont(parameter);
+		fontMedium.setColor(Color.BLACK);
+		// Ahora utiliza freetype font = new BitmapFont();	//Use LibGDX's default Arial font.
+		this.setScreen(new MenuScreen(this)); // fuente en el constructor para mayor facilidad en caso de cambiar la fuente.
 	}
 
 	@Override
@@ -30,7 +49,9 @@ public class Ranero extends Game {
 	@Override
 	public void dispose() {
 		batch.dispose();
-		font.dispose();
+		fontBig.dispose();
+		fontMedium.dispose();
+		generator.dispose();
 	}
 	/*
 
--- desktop/src/es/codemonsters/ranero/desktop/DesktopLauncher.java
+++ desktop/src/es/codemonsters/ranero/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import es.codemonsters.ranero.Ranero;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 700;
 		new LwjglApplication(new Ranero(), config);
 	}
 }
