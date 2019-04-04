95e27bed753d834d6ffa7dbfae3b2fed269a62e1
asexto <a.sexto@gmail.com>
2019-04-04 19:19:41 +0200

    Añadido código inicial de Scene2D para utilizar un FitViewPort

--- core/src/es/codemonsters/ranero/screens/MenuScreen.java
+++ core/src/es/codemonsters/ranero/screens/MenuScreen.java
@@ -5,23 +5,20 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-
-import java.util.Random;
-
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import es.codemonsters.ranero.Ranero;
 
 public class MenuScreen implements Screen {
     final Ranero game;
-
-    OrthographicCamera camera;
-
+    private Stage stage;
+    //private Table table;
 
     public MenuScreen(final Ranero game) {
         this.game = game;
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
-        Gdx.gl.glClearColor(1f, 1f, 1f, 1);
+        stage = new Stage(new FitViewport(100, 100));
+        Gdx.gl.glClearColor(0f, 1f, 1f, 1);
     }
 
     @Override
@@ -33,6 +30,9 @@ public class MenuScreen implements Screen {
     public void render(float delta) {
         game.batch.begin();
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(delta);
+        //stage.draw();
+
         // Fuente obtenida de https://www.fontsquirrel.com/fonts/list/classification/comic, Comic Relief
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
             game.setScreen(new GameScreen(game));
@@ -45,7 +45,7 @@ public class MenuScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -65,6 +65,6 @@ public class MenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
