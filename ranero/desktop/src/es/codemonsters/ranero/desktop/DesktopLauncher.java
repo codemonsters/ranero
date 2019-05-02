725ce521ec10f064c62c00db7ffa745396db7115
PabloHernandez0 <49038740+PabloHernandez0@users.noreply.github.com>
2019-05-02 19:42:00 +0200

    Add files via upload
    Coche añadido

---
+++ ranero/desktop/src/es/codemonsters/ranero/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package es.codemonsters.ranero.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.codemonsters.ranero.Ranero;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 700;
+		new LwjglApplication(new Ranero(), config);
+	}
+}
